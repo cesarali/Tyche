a5a77c7907f65b8a3876577f6d8e8b0b52b7cf53
Kostadin Cvejoski <cvejoski@gmail.com>
2019-10-25 16:34:38 +0200

    remove the loss from the trainer

--- src/tyche/trainer.py
+++ src/tyche/trainer.py
@@ -4,6 +4,7 @@ import logging
 import os
 from abc import ABCMeta, abstractmethod
 from typing import Dict
+import yaml
 
 import torch
 import tqdm
@@ -14,33 +15,34 @@ from .utils.helper import get_device
 
 class BaseTrainingProcedure(metaclass=ABCMeta):
 
-    def __init__(self, model, loss, metrics, optimizer, resume, params, train_logger, data_loader):
+    def __init__(self, model, metrics, optimizer, resume, params, data_loader, train_logger, **kwargs):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.model = model
-        self.loss = loss
         self.metrics = metrics
         self.optimizer = optimizer
         self.params = params
+
         self._prepare_dirs()
+        self.__save_params()
         self.t_logger = self._setup_logging()
         self.summary = SummaryWriter(self.tensorboard_dir)
         self.device = get_device(params)
         self.model.to(self.device)
         self.start_epoch = 0
-        self.n_epochs = self.params["trainer"]["epochs"]
-        self.save_after_epoch = self.params["trainer"]["save_after_epoch"]
-        self.batch_size = self.params["data_loader"]["args"]["batch_size"]
-        self.bm_metric = self.params["trainer"]["args"]['bm_metric']
+        self.n_epochs = self.params['trainer']['epochs']
+        self.save_after_epoch = self.params['trainer']['args']['save_after_epoch']
+        self.batch_size = self.params['data_loader']['args']['batch_size']
+        self.bm_metric = self.params['trainer']['args']['bm_metric']
 
         self.data_loader = data_loader
         self.n_train_batches = len(data_loader.train)
         self.n_val_batches = len(data_loader.validate)
 
         self.global_step = 0
-        self.best_model = {'train_loss': float("inf"),
-                           'val_loss': float("inf"),
-                           'train_metric': float("inf"),
-                           'val_metric': float("inf")}
+        self.best_model = {'train_loss': float('inf'),
+                           'val_loss': float('inf'),
+                           'train_metric': float('inf'),
+                           'val_metric': float('inf')}
 
         self.train_logger = train_logger
         if resume:
@@ -52,27 +54,27 @@ class BaseTrainingProcedure(metaclass=ABCMeta):
 
     def train(self):
         e_bar = tqdm.tqdm(
-                desc="Epoch: ",
+                desc='Epoch: ',
                 total=self.n_epochs,
-                unit="epoch",
+                unit='epoch',
                 initial=self.start_epoch,
-                postfix="train loss: -, validation loss: -")
+                postfix='train loss: -, validation loss: -')
         for epoch in range(self.start_epoch, self.n_epochs):
             train_log = self._train_epoch(epoch)
             validate_log = self._validate_epoch(epoch)
 
             self.__update_p_bar(e_bar, train_log, validate_log)
+            self._check_and_save_best_model(train_log, validate_log)
             if epoch % self.save_after_epoch == 0:
-                self._check_and_save_best_model(train_log, validate_log)
                 self._save_check_point(epoch)
         e_bar.close()
-        self.best_model['name'] = self.params["name"]
+        self.best_model['name'] = self.params['name']
         return self.best_model
 
     def _train_epoch(self, epoch: int) -> Dict:
         self.model.train()
         p_bar = tqdm.tqdm(
-                desc="Training batch: ", total=self.n_train_batches, unit="batch")
+                desc='Training batch: ', total=self.n_train_batches, unit='batch')
 
         epoch_stat = self._new_stat()
         for batch_idx, data in enumerate(self.data_loader.train):
@@ -81,7 +83,7 @@ class BaseTrainingProcedure(metaclass=ABCMeta):
         p_bar.close()
 
         self._normalize_stats(self.n_train_batches, epoch_stat)
-        self._log_epoch("train/epoch/", epoch_stat)
+        self._log_epoch('train/epoch/', epoch_stat)
 
         return epoch_stat
 
@@ -104,36 +106,41 @@ class BaseTrainingProcedure(metaclass=ABCMeta):
         self.summary.close()
 
     def _prepare_dirs(self) -> None:
-        trainer_par = self.params["trainer"]
-        start_time = datetime.datetime.now().strftime("%d%m_%H%M%S")
-        self.checkpoint_dir = os.path.join(trainer_par["save_dir"],
-                                           self.params["name"], start_time)
-        self.logging_dir = os.path.join(trainer_par["logging"]["logging_dir"],
-                                        self.params["name"], start_time)
-        self.tensorboard_dir = os.path.join(trainer_par["logging"]["tensorboard_dir"],
-                                            self.params["name"], start_time)
+        trainer_par = self.params['trainer']
+        start_time = datetime.datetime.now().strftime('%d%m_%H%M%S')
+        self.checkpoint_dir = os.path.join(trainer_par['save_dir'],
+                                           self.params['name'], start_time)
+        self.logging_dir = os.path.join(trainer_par['logging']['logging_dir'],
+                                        self.params['name'], start_time)
+        self.tensorboard_dir = os.path.join(trainer_par['logging']['tensorboard_dir'],
+                                            self.params['name'], start_time)
 
         os.makedirs(self.logging_dir, exist_ok=True)
         os.makedirs(self.checkpoint_dir, exist_ok=True)
         os.makedirs(self.tensorboard_dir, exist_ok=True)
 
+    def __save_params(self):
+        params_path = os.path.join(self.logging_dir, 'config.yaml')
+        self.logger.info(f'saving config into {params_path}')
+        yaml.dump(self.params, open(params_path, 'w'), default_flow_style=False)
+
     def __save_model(self, file_name, **kwargs) -> None:
         model_type = type(self.model).__name__
         state = {
-            "model_type": model_type,
-            "epoch": kwargs.get("epoch"),
-            "model_state": self.model.state_dict(),
-            "optimizer": self.optimizer.state_dict(),
-            "params": self.params
+            'model_type': model_type,
+            'epoch': kwargs.get('epoch'),
+            'model_state': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'params': self.params
         }
         torch.save(state, file_name)
 
     def _save_model_parameters(self, file_name):
-        """
+        '''
         Args:
             file_name:
-        """
-        with open(file_name, "w") as f:
+        '''
+        with open(file_name, 'w') as f:
             json.dump(self.params, f, indent=4)
 
     def _save_check_point(self, epoch: int) -> None:
@@ -146,14 +153,14 @@ class BaseTrainingProcedure(metaclass=ABCMeta):
         """
 
         file_name = os.path.join(self.checkpoint_dir,
-                                 "checkpoint-epoch{}.pth".format(epoch))
-        self.t_logger.info("Saving checkpoint: {} ...".format(file_name))
+                                 'checkpoint-epoch{}.pth'.format(epoch))
+        self.t_logger.info('Saving checkpoint: {} ...'.format(file_name))
         self.__save_model(file_name, epoch=epoch)
 
     def _save_best_model(self) -> None:
         file_name = os.path.join(self.checkpoint_dir,
-                                 "best_model.pth")
-        self.t_logger.info("Saving best model ...")
+                                 'best_model.pth')
+        self.t_logger.info('Saving best model ...')
         self.__save_model(file_name)
 
     def _resume_check_point(self, path: str) -> None:
@@ -164,23 +171,23 @@ class BaseTrainingProcedure(metaclass=ABCMeta):
         :rtype:
 
         """
-        self.logger.info("Loading checkpoint: {} ...".format(path))
+        self.logger.info('Loading checkpoint: {} ...'.format(path))
         state = torch.load(path)
-        self.params = state["params"]
-        self.start_epoch = state["epoch"] + 1
-        self.model.load_state_dict(state["model_state"])
-        self.optimizer.load_state_dict(state["optimizer"])
-        self.logger.info("Finished loading checkpoint: {} ...".format(path))
+        self.params = state['params']
+        self.start_epoch = state['epoch'] + 1
+        self.model.load_state_dict(state['model_state'])
+        self.optimizer.load_state_dict(state['optimizer'])
+        self.logger.info('Finished loading checkpoint: {} ...'.format(path))
 
     def _setup_logging(self) -> logging.Logger:
-        logger = logging.getLogger("train_logger")
+        logger = logging.getLogger('train_logger')
         logger.propagate = False
         logger.setLevel(logging.INFO)
 
-        file_name = os.path.join(self.logging_dir, "train.log")
+        file_name = os.path.join(self.logging_dir, 'train.log')
         fh = logging.FileHandler(file_name)
         formatter = logging.Formatter(
-                self.params["trainer"]["logging"]["formatters"]["simple"])
+                self.params['trainer']['logging']['formatters']['simple'])
         fh.setLevel(logging.INFO)
 
         fh.setFormatter(formatter)
@@ -190,27 +197,27 @@ class BaseTrainingProcedure(metaclass=ABCMeta):
 
     def _log_train_step(self, epoch: int, batch_idx: int, logs: Dict, batch_size: int) -> None:
         data_len = len(self.data_loader.train.dataset)
-        log = self.__build_raw_log_str("Train epoch", batch_idx, epoch, logs, data_len, batch_size)
+        log = self.__build_raw_log_str('Train epoch', batch_idx, epoch, logs, data_len, batch_size)
         self.t_logger.info(log)
         for k, v in logs.items():
             self.summary.add_scalar('train/batch/' + k, v, self.global_step)
 
     def _log_validation_step(self, epoch: int, batch_idx: int, logs: Dict, batch_size: int) -> None:
         data_len = len(self.data_loader.validate.dataset)
-        log = self.__build_raw_log_str("Validation epoch", batch_idx, epoch, logs, data_len, batch_size)
+        log = self.__build_raw_log_str('Validation epoch', batch_idx, epoch, logs, data_len, batch_size)
         self.t_logger.info(log)
         # for k, v in logs.items():
         #     self.summary.add_scalar('validate/batch/' + k, v, self.global_step)
 
     @staticmethod
     def __build_raw_log_str(prefix: str, batch_idx: int, epoch: int, logs: Dict, data_len: int, batch_size: int):
-        sb = prefix + ": {} [{}/{} ({:.0%})]".format(
+        sb = prefix + ': {} [{}/{} ({:.0%})]'.format(
                 epoch,
                 batch_idx * batch_size,
                 data_len,
                 100.0 * batch_idx / data_len)
         for k, v in logs.items():
-            sb += " {}: {:.6f}".format(k, v)
+            sb += ' {}: {:.6f}'.format(k, v)
         return sb
 
     def _check_and_save_best_model(self, train_log: Dict, validate_log: Dict) -> None:
