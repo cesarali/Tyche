f79628b2a4c65af1ab7ce6952f35b420eeb8e4bf
cesarali <cesarali07@gmail.com>
2019-12-06 16:59:09 +0100

    validate epoch method

--- src/tyche/models.py
+++ src/tyche/models.py
@@ -42,6 +42,15 @@ class AModel(nn.Module, ABC):
     def validate_step(self, minibatch: Any) -> Dict:
         raise NotImplementedError("The validate_step method is not implemented in your class!")
 
+    def validate_epoch(self,dataloader: Any = None, epoch: int = 1) -> Dict:
+        """
+        This is performed only once every
+            save_after_epoch: 10
+            and usually accounts for global validation metrics, like the evaluation of the full graph etc
+        :return:
+        """
+        return {}
+
     @property
     def device(self):
         return next(self.parameters()).device
